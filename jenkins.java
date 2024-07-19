b3dc7972bbf5c985fbcf451edb9891d3f8a64aee
Sabari300 <147585493+Sabari300@users.noreply.github.com>
2024-07-19 00:33:17 -0700

    Create jenkins.java

---
+++ jenkins.java
@@ -0,0 +1,7 @@
+class jenkins{  
+    public static void main(String args[]){  
+     System.out.println("Hi sabari you r doing great"); 
+     System.out.println("fuck all"); 
+     System.out.println("god of the world"); 
+    }  
+}  
